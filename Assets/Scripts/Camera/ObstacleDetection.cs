5bf4122fe3b21667d5802a989b7ab5bf99b992fd
Jeremy <lesage.jeremy.fr@gmail.com>
2018-12-07 17:49:29 +0100

    Dissolve Obstacle Player-Camera (blocking version)

---
+++ Assets/Scripts/Camera/ObstacleDetection.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO : need improvment to get the correct material back and not instanced material
+public class ObstacleDetection : MonoBehaviour
+{
+    [SerializeField] Texture _dissolveMask;
+    [Range (0f,1f)]
+    [SerializeField] float _cutout = .5f;
+
+    private int _layerMask = 1 << 16;
+
+    private string _dissolveName = "Custom/SHADER_DissolveObstacle";
+    private string _standardName = "Standard";
+
+    private Shader _dissolveShader;
+    private Shader _standardShader;
+
+    void Start ()
+    {
+        _dissolveShader = Shader.Find(_dissolveName);
+        _standardShader = Shader.Find(_standardName);
+    }
+
+    private bool CheckLayerMask(int layer)
+    {
+        return _layerMask == (_layerMask | (1 << layer));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (CheckLayerMask(other.gameObject.layer))
+        {
+            CheckRenderer(other, _dissolveShader);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (CheckLayerMask (other.gameObject.layer))
+        {
+            CheckRenderer(other, _standardShader);
+        }
+    }
+
+    private void CheckRenderer (Collider other, Shader shader)
+    {
+        MeshRenderer[] rend = other.GetComponentsInChildren<MeshRenderer>();
+        foreach (MeshRenderer r in rend)
+        {
+            SwitchShader(r, shader);
+        }
+    }
+
+    private void SwitchShader(MeshRenderer meshRenderer, Shader shader)
+    {
+        Material[] mat = meshRenderer.materials;
+        foreach (Material m in mat)
+        {
+            m.shader = shader;
+            if (shader == _dissolveShader)
+            {
+                m.SetTexture("_DissolveTex", _dissolveMask);
+                m.SetFloat("_Cutout", _cutout);
+            }
+        }
+        meshRenderer.materials = mat;
+    }
+}
---
+++ Assets/Shaders/SHADER_DissolveObstacle.shader
@@ -0,0 +1,50 @@
+﻿// basic and fast version
+// TODO : need improvment to share the dissolve mask
+Shader "Custom/SHADER_DissolveObstacle" {
+	Properties {
+		_Color ("Color", Color) = (1,1,1,1)
+		_MainTex ("Albedo (RGB)", 2D) = "white" {}
+		_Glossiness ("Smoothness", Range(0,1)) = 0.5
+		_Metallic ("Metallic", Range(0,1)) = 0.0
+
+		[Header (Dissolve)]
+		_DissolveTex ("Mask Dissolve", 2D) = "white" {}
+		_Cutout ("Cutout",  Range(0,1)) = 0
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+		LOD 200
+
+		CGPROGRAM
+		#pragma surface surf Standard fullforwardshadows
+		#pragma target 3.0
+
+		struct Input {
+			float2 uv_MainTex;
+			float4 screenPos;
+		};
+
+		sampler2D _MainTex;
+		sampler2D _DissolveTex;
+
+		half _Glossiness;
+		half _Metallic;
+		half _Cutout;
+
+		fixed4 _Color;
+
+		void surf (Input IN, inout SurfaceOutputStandard o) {
+			fixed4 c = tex2D (_MainTex, IN.uv_MainTex) * _Color;
+
+			half2 uv = IN.screenPos.xy / IN.screenPos.w;
+			fixed m = tex2D(_DissolveTex, uv).r;
+
+			o.Albedo = c.rgb;
+			o.Metallic = _Metallic;
+			o.Smoothness = _Glossiness;
+			clip(m - _Cutout);
+		}
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
