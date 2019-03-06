3cc2231621454f26dbef81c8b0e8b2e6913a1ae9
François Hautier <francoishautier+gitkraken@protonmail.ch>
2019-03-06 21:25:35 +0100

    Little commit to save

---
+++ Assets/Scripts/Player/PlayerClimbing.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerClimbing : MonoBehaviour
+{
+    public Collider Collider;
+    
+    public void OnDrawGizmos()
+    {
+        Vector3 closestPoint = Collider.ClosestPoint(transform.position);
+
+        Gizmos.DrawWireSphere(closestPoint, 0.1f);
+    }
+}
