d1f0ff8dd4f72e65053689a7318c3e7dece3402e
FranSOSome <francois.hautier@osome-studio.com>
2019-03-07 13:59:27 +0100

    wip

--- Assets/Scripts/Player/PlayerClimbing.cs
+++ Assets/Scripts/Player/PlayerClimbing.cs
@@ -6,10 +6,30 @@ public class PlayerClimbing : MonoBehaviour
 {
     public Collider Collider;
     
+    public float radius = 0.5f;
+    
     public void OnDrawGizmos()
     {
         Vector3 closestPoint = Collider.ClosestPoint(transform.position);
 
+        Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(closestPoint, 0.1f);
+        
+        Vector3 normalMove = transform.position - closestPoint;
+        
+        Gizmos.DrawRay(closestPoint, normalMove);
+        Gizmos.DrawWireSphere(closestPoint + normalMove.normalized * radius, radius);
+        
+        
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(transform.position, transform.forward);
+
+
+        Quaternion rotation = Quaternion.FromToRotation(Vector3.up, normalMove);
+
+        Vector3 moveDirectionProject = rotation * transform.forward;
+        
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(closestPoint, moveDirectionProject.normalized);
     }
 }
