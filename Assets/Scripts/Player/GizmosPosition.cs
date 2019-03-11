2149b78066cf81fbe5f76b9ab4a97bdbbe9ce9f3
FranSOSome <francois.hautier@osome-studio.com>
2019-03-11 13:48:23 +0100

    Character controller research

---
+++ Assets/Scripts/Player/GizmosPosition.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GizmosPosition : MonoBehaviour
+{
+    List<Vector3> allPositions = new List<Vector3>();
+    Vector3 previousSavePosition;
+
+    private void OnDrawGizmos ()
+    {
+        for (int i = 0; i < allPositions.Count; i++)
+        {
+            Gizmos.color = Color.white;
+            Gizmos.DrawWireSphere(allPositions[i], 0.01f);
+            
+            if(i == 0)
+            {
+                continue;
+            }
+
+            Gizmos.DrawLine(allPositions[i-1], allPositions[i]);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if(Input.GetKeyDown(KeyCode.F1))
+        {
+            allPositions.Clear();
+        }
+
+        if (previousSavePosition != transform.position)
+        {
+            previousSavePosition = transform.position;
+            allPositions.Add(transform.position);
+            
+            while (allPositions.Count > 300)
+            {
+                allPositions.RemoveAt(0);
+            }
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,24 +1,21 @@
 ﻿using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
-[RequireComponent(typeof(CapsuleCollider))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private float moveSpeed = 5f;
-    
-    private Rigidbody _rb;
 
+    [SerializeField]
+    private float radius;
+
+    [SerializeField]
+    private float groundCheckDistance = 1f;
+    
     private Vector3 _direction;
 
     private Vector2 _inputDirection;
     
     public Vector3 Direction => _direction;
-
-    private void Awake()
-    {
-        _rb = GetComponent<Rigidbody>();
-    }
 
     private void OnEnable()
     {
@@ -43,7 +40,7 @@ public class PlayerController : MonoBehaviour
         Debug.Log("jump soon!");
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         // TODO temp
         Vector3 inputDirection = new Vector3(
@@ -55,15 +52,80 @@ public class PlayerController : MonoBehaviour
         {
             inputDirection = inputDirection.normalized;
         }
-
-        _direction = Quaternion.Euler(new Vector3(0f, Camera.main.transform.eulerAngles.y, 0f)) * inputDirection;
         
-        _rb.velocity = _direction * moveSpeed;
+        _direction = Quaternion.Euler(new Vector3(0f, Camera.main.transform.eulerAngles.y, 0f)) * inputDirection;
+        Debug.DrawRay(transform.position, _direction * 2f, Color.green);
+        
+        /* ---------------- */
+
+        Vector3 groundNormal = Vector3.up;
+        Vector3 groundPos = Vector3.up;
+        
+        /* ---------------- */
+
+        {
+            Vector3 capsuleBottom = GetCapsuleBottom();
+
+            Collider[] overlapCapsule = Physics.OverlapCapsule(
+                capsuleBottom,
+                capsuleBottom - Vector3.up * groundCheckDistance,
+                radius/*,
+                LayerMask.NameToLayer("Ground"), // TODO WTF? pourquoi ca marche pas?
+                QueryTriggerInteraction.Ignore*/);
+
+            float minDistance = float.MaxValue;
+            Collider closestCollider = null; // TODO : handle when there are no colliders
+            Vector3 closestPoint = Vector3.zero; // same
+            
+            foreach (Collider coll in overlapCapsule)
+            {
+                Vector3 newClosestPoint = coll.ClosestPoint(capsuleBottom);
+
+                float distance = Vector3.Distance(newClosestPoint, capsuleBottom); // TODO use sqrMagnitude instead
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    closestCollider = coll;
+                    closestPoint = newClosestPoint;
+                }
+            }
+
+            if (closestCollider == null)
+            {
+                Debug.LogError("NO COLLIDER FOR GROUND");
+            }
+
+            groundNormal = (capsuleBottom - closestPoint).normalized;
+            transform.position = closestPoint + groundNormal * radius - Vector3.up * radius; // ATTENTION A PAS LE POUSSER DANS UN AUTRE COLL
+            
+            Debug.DrawRay(closestPoint, groundNormal * 2f, Color.magenta);
+        }
+
+        /* ---------------- */
+        
+        Quaternion rotation = Quaternion.FromToRotation(Vector3.up, groundNormal);
+        Vector3 moveDirectionProject = rotation * _direction;
+        
+        Debug.DrawRay(transform.position, moveDirectionProject * 2f, Color.yellow);
+        
+        transform.position += moveDirectionProject * moveSpeed * Time.deltaTime; // le mouvement doit jamais être supérieur au radius pour éviter des problème de pénétration
 
         if (_direction.sqrMagnitude > 0.2f)
         {
             transform.forward = -_direction; // TODO ultra temp wtf
         }
         
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(GetCapsuleBottom(), radius);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(GetCapsuleBottom() - Vector3.up * groundCheckDistance, radius);
+    }
+
+    private Vector3 GetCapsuleBottom()
+    {
+        return transform.position + Vector3.up * radius;
     }
 }
